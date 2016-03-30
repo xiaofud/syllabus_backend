d9c59b60666cd51aa000ead031b08ab716b40b15
hjsmallfly <szu_nwad@icloud.com>
2016-03-30 12:53:26 +0800

    add wsgi script and ini config file

---
+++ wsgi.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+__author__ = 'smallfly'
+
+import sys
+import logging
+import os
+logging.basicConfig(stream=sys.stderr)
+sys.path.insert(0, os.path.dirname(__file__))
+
+# application 是 uwsgi 所需的默认名字
+from app import app as application
+if __name__ == "__main__":
+    # 记得把 debug 关闭
+	application.run(host="0.0.0.0", port=8080, debug=False)
+
