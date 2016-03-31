a455eaa8361ba98be76436d25eb9c18ebbbf6105
hjsmallfly <szu_nwad@icloud.com>
2016-03-31 22:36:34 +0800

    changed the constants for post type

--- app/mod_interaction/models.py
+++ app/mod_interaction/models.py
@@ -76,10 +76,10 @@ class User(db.Model):
 class Post(db.Model):
 
     # 话题, 用户自发的
-    POST_TYPE_TOPIC = 1
+    POST_TYPE_TOPIC = 0
 
     # 宣传活动性质的(公众号推文)
-    POST_TYPE_ACTIVITY = 2  # 如果是这种类型的话, 那么客户端处理的时候就要注意把content作为文章的URL
+    POST_TYPE_ACTIVITY = 1  # 如果是这种类型的话, 那么客户端处理的时候就要注意把content作为文章的URL
 
     __tablename__ = "posts"
 
