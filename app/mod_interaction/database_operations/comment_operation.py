99b18ccc49a7bc4d5ab7b619abe5cdf1f7d599ab
hjsmallfly <szu_nwad@icloud.com>
2016-05-28 22:44:45 +0800

    fix bug: notify the author of the post

--- app/mod_interaction/database_operations/comment_operation.py
+++ app/mod_interaction/database_operations/comment_operation.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 __author__ = 'smallfly'
 
-from app.mod_interaction.models import Comment, UnRead
+from app.mod_interaction.models import Comment, UnRead, Post
 from app import db
 
 def new_unread(post_id, comment_user_id):
@@ -17,9 +17,18 @@ def new_unread(post_id, comment_user_id):
     # 找到所有先前评论过 post_id 所指向的post
     # return model.query.with_entities(model.id).order_by(model.id.desc()).first().id
     comments = Comment.query.with_entities(Comment.uid).filter_by(post_id=post_id).all()
+    post = Post.query.with_entities(Post.uid).filter_by(id=post_id).first()
+    if post is None:
+        return False
+    # 如果不是作者本人发表的这次评论, 那么通知作者
+    if post.uid != comment_user_id:
+        print("notifying author")
+        db.session.add(UnRead(uid=post.uid, post_id=post_id))
 
+    # 通知其他评论用户
     for comment in comments:
-        if comment.uid != comment_user_id:
+        # 不通知发表这次评论的人, 注意不要重复发送通知
+        if comment.uid != comment_user_id and comment.uid != post.uid:
             unread = UnRead(uid=comment.uid, post_id=post_id)
             print('add unread for user', comment.uid)
             print('the unread:', repr(unread))
