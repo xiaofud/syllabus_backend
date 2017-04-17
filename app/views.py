5a057c7cf4319797a593d57be0a8f66de8cb5245
xiaofu <szu_nwad@icloud.com>
2017-04-17 21:46:35 +0800

    views.py: Cancel view for /oauth2 Empty location when deployed behind Nginx.

--- app/views.py
+++ app/views.py
@@ -13,23 +13,25 @@ def index():
 def version():
     return render_template("version.html", version=helpers.load_version())
 
-ANDROID_REDIRECT_URI = 'stu://syllabus:80/login'
-IOS_REDIRECT_URI = 'stuclass://'
-
-# curl http://localhost:8080/oauth2_android?code=1234 -i
-@app.route("/oauth2_android", methods=['GET'])
-def oauth2_android():
-    if request.method == 'GET':
-        if "code" not in request.args:
-            print("Missing code in args")
-            return jsonify(msg="bad request"), 400
-        return redirect(ANDROID_REDIRECT_URI + "?code=" + request.args["code"])
-
-# curl http://localhost:8080/oauth2_ios?code=1234 -i
-@app.route("/oauth2_ios", methods=['GET'])
-def oauth2_ios():
-    if request.method == 'GET':
-        if "code" not in request.args:
-            print("Missing code in args")
-            return jsonify(msg="bad request"), 400
-        return redirect(IOS_REDIRECT_URI + "?code=" + request.args["code"])
+# Empty location when deployed behind Nginx
+# Not sure the reason yet. Use Nginx server for a workaround now.
+# ANDROID_REDIRECT_URI = 'stu://syllabus:80/login'
+# IOS_REDIRECT_URI = 'stuclass://'
+#
+# # curl http://localhost:8080/oauth2_android?code=1234 -i
+# @app.route("/oauth2_android", methods=['GET'])
+# def oauth2_android():
+#     if request.method == 'GET':
+#         if "code" not in request.args:
+#             print("Missing code in args")
+#             return jsonify(msg="bad request"), 400
+#         return redirect(ANDROID_REDIRECT_URI + "?code=" + request.args["code"])
+#
+# # curl http://localhost:8080/oauth2_ios?code=1234 -i
+# @app.route("/oauth2_ios", methods=['GET'])
+# def oauth2_ios():
+#     if request.method == 'GET':
+#         if "code" not in request.args:
+#             print("Missing code in args")
+#             return jsonify(msg="bad request"), 400
+#         return redirect(IOS_REDIRECT_URI + "?code=" + request.args["code"])
