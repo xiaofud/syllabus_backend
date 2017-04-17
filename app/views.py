3fb0288e0326d7fbc274587359c326d4e3b91c53
xiaofu <szu_nwad@icloud.com>
2017-04-17 13:37:35 +0800

    views.py: 加入OAUTH2回调

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 __author__ = 'smallfly'
 
-from flask import render_template
+from flask import render_template, request, jsonify, redirect
 from app import app
 from app.mod_interaction.resources import helpers
 
@@ -12,3 +12,24 @@ def index():
 @app.route("/version")
 def version():
     return render_template("version.html", version=helpers.load_version())
+
+ANDROID_REDIRECT_URI = 'stu://syllabus:80/login'
+IOS_REDIRECT_URI = 'stuclass://'
+
+# curl http://localhost:8080/oauth2_android?code=1234 -i
+@app.route("/oauth2_android", methods=['GET'])
+def oauth2_android():
+    if request.method == 'GET':
+        if "code" not in request.args:
+            print("Missing code in args")
+            return jsonify(msg="bad request"), 400
+        return redirect(ANDROID_REDIRECT_URI + "?code=" + request.args["code"])
+
+# curl http://localhost:8080/oauth2_ios?code=1234 -i
+@app.route("/oauth2_ios", methods=['GET'])
+def oauth2_ios():
+    if request.method == 'GET':
+        if "code" not in request.args:
+            print("Missing code in args")
+            return jsonify(msg="bad request"), 400
+        return redirect(IOS_REDIRECT_URI + "?code=" + request.args["code"])
